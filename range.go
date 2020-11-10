7e77547ae7fe605d9ddb2776e36b711042dd0cf8
sumit-awasthi <72277800+sumit-awasthi@users.noreply.github.com>
2020-11-10 16:58:07 +0530

    updates

--- range.go
+++ range.go
@@ -5,10 +5,10 @@ import "fmt"
 
 func main() {
 	var a []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	//for i:=0;i< len(a);i++{
+	//for i:=0;i< len(a);i++{  //normal printing of elements be like
 	//fmt.Println(a[i])
 	//	}
-	for i, element := range a {
-		fmt.Printf("%d:%d\n", i, element)
+	for i, element := range a { //here i is the index and element is the stored item inside the brackets
+		fmt.Printf("%d:%d\n", i, element) // to display content without the index value replace i with _(for _,elements:=range a     fmt.printf("%d\n",elements))
 	}
 }
