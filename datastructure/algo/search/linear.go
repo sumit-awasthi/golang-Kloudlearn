83b89a9ce31a3b8bd232643333aca2c9cfe570a5
sumit-awasthi <72277800+sumit-awasthi@users.noreply.github.com>
2020-11-24 15:27:33 +0530

    .

--- datastructure/algo/search/linear.go
+++ datastructure/algo/search/linear.go
@@ -1,8 +1,10 @@
 //Linear search
 package main
+
 import (
 	"fmt"
 )
+
 func LinearSearch(elements []int, findElement int) bool {
 	var element int
 	for _, element = range elements {
@@ -14,7 +16,8 @@ func LinearSearch(elements []int, findElement int) bool {
 }
 func main() {
 	var elements []int
-	elements = []int{15, 48, 26, 18, 41, 86, 29, 51, 20}
+	elements = []int{15, 48, 26, 18, 41, 86, 29, 51, 20, 45, 49, 55}
 	fmt.Println(elements)
 	fmt.Println(LinearSearch(elements, 48))
+	fmt.Printf("found element %d at index %d in %v\n", element, i, elements)
 }
